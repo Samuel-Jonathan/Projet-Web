7bec779c4d88173c6ee013b1e0698fd6d2a6c69e
Ralph-Mohdad <ralph.modad06@gmail.com>
2023-02-10 17:27:10 +0100

    #62

--- casse-brique/gameloop.js
+++ casse-brique/gameloop.js
@@ -1,10 +1,11 @@
 import Paddle from "./paddle.js";
 import Ball from "./ball.js";
+import Brick from "./bricks.js";
 import { circRectsOverlap } from "./collisions.js";
 
-
+let tabBricks = [];
 let canvas, ctx;
-let paddle, ball;
+let paddle, ball, brick;
 
 window.onload = init;
 
@@ -15,6 +16,8 @@ function init() {
     ctx = canvas.getContext("2d");
     paddle = new Paddle(75, canvas.height - 50, 100, 10, 10, "blue");
     ball = new Ball(canvas.width / 2, canvas.height - 80, 10, "red", 6, -6);
+    brick = new Brick(100, 100, 100, 10, "black");
+    createBricks();
     document.addEventListener("keydown", paddle.handleKeyDown.bind(paddle));
     document.addEventListener("keyup", paddle.handleKeyUp.bind(paddle));
     requestAnimationFrame(gameLoop);
@@ -28,6 +31,11 @@ function gameLoop(time) {
     // 2 on dessinea
     ball.draw(ctx);
     paddle.draw(ctx);
+    for (let i = 0; i < tabBricks.length; i++) {
+        tabBricks[i].draw(ctx);
+
+
+    }
    
     // 3 on met à jour
     paddle.update(canvas.width);
@@ -63,6 +71,20 @@ function handleCollisionBallPaddle() {
     }
 }
 
+function createBricks() {
+    for (let l = 0; l < 9; l++) { // iterate 9 times
+        for (let c = 0; c < 9; c++) { // iterate 9 times
+            let x = l * 110; // x = 0, 110, 220, 330, 440, 550, 660, 770, 880
+            let y = c * 20; // y = 0, 20, 40, 60, 80, 100, 120, 140, 160
+            let width = 100;
+            let height = 10;
+            let color = "blue";
+            let b = new Brick(x, y, width, height, color);
+            tabBricks.push(b);
+            console.log(tabBricks);
+        }
+    }
+}
 
 
 
