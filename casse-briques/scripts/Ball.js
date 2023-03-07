6db3395b8385669f6fdf116a56d5c1a56522f815
Samuel-Jonathan <jtn.samuel@gmail.com>
2023-03-07 15:11:53 +0100

    Refactoring (affichage, arborescence des fichiers) #55

---
+++ casse-briques/scripts/Ball.js
@@ -0,0 +1,72 @@
+import { circRectsOverlap } from "./Collisions.js";
+
+export default class Ball {
+    constructor(x, y, radius, color, dx, dy) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.dx = dx;
+        this.dy = dy;
+    }
+    
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.beginPath();
+        ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+    }
+    
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    getRadius(){
+        return this.radius;
+    }
+    
+
+
+    update(width, height) {
+        this.x += this.dx;
+        this.y += this.dy;
+        this.handleCanvasCollision(width, height);
+    }
+    /*
+    handleCanvasCollision(width, height) {
+        if (this.x + this.dx > width - this.radius || this.x + this.dx < this.radius) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.dy < this.radius || this.y + this.dy > height - this.radius) {
+            this.dy = -this.dy;
+        }
+    }
+    */
+    handleCanvasCollision(width, height) {
+        if (this.x + this.dx > width - this.radius || this.x + this.dx < this.radius) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.dy < this.radius) {
+            this.dy = -this.dy;
+        } else if (this.y + this.dy > height - this.radius) {
+            // Si la balle touche le bas du canvas
+            if (this.y + this.dy >= height - this.radius) {
+                // Afficher le message "Game Over"
+                // alert("Game Over");
+                // Relancer le jeu
+                document.location.reload();
+            }
+        }
+    }
+
+    
+
+}
---
+++ casse-briques/scripts/PaddleBonus.js
@@ -0,0 +1,41 @@
+import { circRectsOverlap } from "./Collisions.js";
+
+export default class PaddleBonus {
+  constructor(x, y, width, height) {
+    this.x = random(50, 950);
+    this.y = random(300, 500);
+    this.width = width;
+    this.height = height;
+
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.beginPath();
+    ctx.fillRect(0, 0, this.width, this.height);
+    ctx.closePath();
+    ctx.restore();
+  }
+
+  /*collision(ctx,ballX,ballY,ballRadius){
+    if (circRectsOverlap(this.x, this.y, this.width, this.height, ballX, ballY, ballRadius)) {
+      ctx.clearRect(0,0,this.width, this.height);
+      return true;
+    }
+    return false;
+  }
+  */
+  collision(ball) {
+    if (ball.x + ball.size > this.x && ball.x < this.x + this.width && ball.y + ball.size > this.y && ball.y < this.y + this.height) {
+      ctx.clearRect(this.x, this.y, this.width, this.height);
+      return true;
+    }
+    return false;
+  }
+
+}
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
