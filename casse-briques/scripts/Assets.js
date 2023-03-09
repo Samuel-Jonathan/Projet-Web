6a79eda56d5938283070b68979d9623456bb6858
Samuel-Jonathan <jtn.samuel@gmail.com>
2023-03-09 08:59:52 +0100

    Refactoring #55

--- casse-briques/scripts/Assets.js
+++ casse-briques/scripts/Assets.js
@@ -1,18 +1,15 @@
-// Raquette
-export var paddleImg = new Image(95, 22)
-paddleImg.src = "assets/paddle.png";
 
 // Game Over
-export var gameOverImg = new Image(324,226);
+export var gameOverImg = new Image(324, 226);
 gameOverImg.src = "assets/gameover.png";
 
 // Bouton pour rejouer
-export var replayButtonImg = new Image(100,100);
+export var replayButtonImg = new Image(100, 100);
 replayButtonImg.src = "assets/replay_button.png";
 
 
 // Bouton pour rejouer (survol)
-export var replayButtonHoverImg = new Image(100,100);
+export var replayButtonHoverImg = new Image(100, 100);
 replayButtonHoverImg.src = "assets/replay_button_hover.png";
 export var paddleImg = new Image(75, 15)
 paddleImg.src = "assets/paddlee.png";
